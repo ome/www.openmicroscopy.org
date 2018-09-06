6c46fb55e434683e56489adbb3bee6ad3adefed0
William Moore <w.moore@dundee.ac.uk>
2018-09-06 16:17:07 +0100

    Add 'Overview' submenu links and preventDefault() for top links

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 $(document).foundation()
+
+$(function(){
+	// We don't want main menu top links to be 'active' as links
+	// We can't replace the <a> because foundation uses them for collapsible menu on mobile
+	// and we can't set href="#" since the travis build fails.
+	$("#main-menu .has-submenu>a").click(event => event.preventDefault());
+});
