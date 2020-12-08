6641c5f62ef3ab0bd58df5a070e7b8967ef89cce
Sebastien Besson <seb.besson@gmail.com>
2020-12-08 15:17:46 +0000

    Add file existence check to build.sh

--- build.sh
+++ build.sh
@@ -8,3 +8,7 @@ docker run --rm -v $PWD:/srv/jekyll -eJEKYLL_UID=$UID jekyll/builder:pages jekyl
 # Report 4xx status codes except 429 errors (Too Many Requests)
 # typically sent by GitHub while linkchecking the downloads
 docker run --rm -v $(pwd)/_site:/site jekyll/builder:pages /usr/gem/bin/htmlproofer /site --only_4xx --http-status-ignore "429" --file_ignore '/minutes/'
+
+# Test file existence
+test -f _site/Schemas/OME/2016-06/ome.xsd
+test -f _site/minutes/README.md
