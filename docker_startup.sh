02679c977d24a758e2aa80b552ecb08cb5c5236e
pwalczysko <p.walczysko@dundee.ac.uk>
2023-02-17 11:19:11 +0000

    Fix docker command similar to idr.op...py.org

--- docker_startup.sh
+++ docker_startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm -p 4000:4000 -v $PWD:/srv/jekyll jekyll/builder:pages jekyll server -w
+docker run -it --rm -p 4000:4000 -v $PWD:/srv/jekyll jekyll/builder:4.2.0 jekyll server -w
